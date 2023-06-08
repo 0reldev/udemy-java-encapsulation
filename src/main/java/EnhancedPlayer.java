b1c1f17c90fc45106fc019f88bb71df6ab990c2c
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-08 11:37:30 +0200

    Lesson part 2

---
+++ src/main/java/EnhancedPlayer.java
@@ -0,0 +1,40 @@
+public class EnhancedPlayer {
+
+    private String fullName;
+    private int healthPercentage;
+    private String weapon;
+
+    public EnhancedPlayer(String fullName) {
+        this(fullName, 100, "Sword)");
+    }
+
+    public EnhancedPlayer(String fullName, int health, String weapon) {
+        this.fullName = fullName;
+        if (health <= 0)
+            this.healthPercentage = 1;
+        else if (health > 100)
+            this.healthPercentage = 100;
+        else
+            this.healthPercentage = health;
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        healthPercentage -= damage;
+        if (healthPercentage <= 0) {
+            System.out.println("Player knocked out of the game.");
+        }
+    }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        healthPercentage += extraHealth;
+        if (healthPercentage > 100) {
+            System.out.println("Player restored to 100%.");
+            healthPercentage = 100;
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,5 +13,8 @@ public class Main {
         player.health = 200;
         player.loseHealth(11);
         System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200, "Sword");
+        System.out.println("Initial health is " +  tim.healthRemaining());
     }
 }
